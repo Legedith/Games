3b40de632c3c03a030104ec9aff9eaeed1fb0e35
Legedith <jatindehmiwal@gmail.com>
2019-08-26 12:11:21 +0530

    changes to story time

--- storyTime.py
+++ storyTime.py
@@ -5,5 +5,5 @@ Created on Fri Sep 21 16:18:56 2018
 @author: DSC
 """
 
-STORY = "This morning {1} woke up feeling {0}. 'It is going to be a {0} day!' Outside, a bunch of {3}s were protesting to keep {2} in stores. They began to {1} to the rhythm of the {0}, which made all the {2}s very {1}. Concerned, {0} texted {2}, who flew {1} to {3} and dropped {1} in a puddle of frozen {2}. {3} woke up in the year {0}, in a world where {3}s ruled the world."
-print(STORY.format(input("Enter adjective:"),input("Enter personal_Noun:"),input("Enter Noun:"),input("Enter character")))
+STORY = "This morning {1} woke up feeling {0}. 'It is going to be a {0} day!' Outside, a bunch of {3}s were protesting to keep {2} in stores. They began to {5} to the rhythm of the {6}, which made all the {2}s very {1}. Concerned, {0} texted {7}, who flew {1} to {3} and dropped {6} in a puddle of frozen {2}. {3} woke up in the year {4}, in a world where {7}s ruled the world."
+print(STORY.format(input("Enter adjective:"),input("Enter personal_Noun:"),input("Enter Noun:"),input("Enter character"),input("Enter Action: "), input("Enter object: "), input("Enter character"), input("Enter YOur Best friend's name")))
